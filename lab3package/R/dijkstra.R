1db49557069080e8d40471ad7a352dc816a55133
lepzh903 <lepeng0401@gmail.com>
2023-09-12 11:44:52 +0200

    Document the package using roxygen2

--- lab3package/R/dijkstra.R
+++ lab3package/R/dijkstra.R
@@ -1,3 +1,23 @@
+#' Find the shortest paths between nodes in a weighted graph
+#' 
+#' @description
+#' The function takes a graph and an initial node and calculates the shortest path from the initial node to every other node in the graph. 
+#' 
+#' @param graph  A data.frame with three variables (v1, v2 and w) that contains the edges of the graph (from v1 to v2) with the weight of the edge (w).
+#' @param init_node A numeric scalar that exists in the graph.
+#'
+#' @returns A numeric vector that consists of the shortest path to every other node from the starting node.
+#' 
+#' @references [link url:](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
+#' 
+#' @examples
+#' wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#'                          v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#'                          w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#' dijkstra(wiki_graph, 1)
+#' dijkstra(wiki_graph, 3)
+#' 
+#' @export
 dijkstra <-
 function(graph, init_node){
     if (!(is.data.frame(graph))){
--- lab3package/R/euclidean.R
+++ lab3package/R/euclidean.R
@@ -1,7 +1,23 @@
+#' Find the greatest common divisor of two numbers
+#' 
+#' @description
+#' This is an efficient function for computing the greatest common divisor (GCD) of two integers (numbers) a and b.
+#' The arguments a and b can not be 0 because 0 has no divisor.
+#' 
+#' @param a,b numeric scalars or integers.
+#'
+#' @returns A number that is the greatest common divisor of a and b.
+#' 
+#' @references [link url:](https://en.wikipedia.org/wiki/Euclidean_algorithm)
+#' 
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+#' @export
 euclidean <-
 function(a, b){
     if (!(is.numeric(a)&is.numeric(b))){
-        stop('Args should be numeric scalars')
+        stop('the arguments should be numeric scalars or integers')
     }
     if (a == 0 || b == 0){
         stop('0 has no divisor')
