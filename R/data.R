4ea7bfd4cc3d5a9175627d05b6b637aa66a1f0a5
lepzh903 <lepeng0401@gmail.com>
2023-09-13 21:32:31 +0200

    include dataset in package

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Data to showcase graph for Dijkstras algorithm
+#' @description
+#' (v1=1,v2=2,w=7) means that the length of the edge linking vertex 1 and vertex 2 is 7.
+#' (v1=6,v2=5,w=9) means that the length of the edge linking vertex 6 and vertex 5 is 9.
+#' @references https://en.wikipedia.org/wiki/Graph
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#' \item{v1}{a vertex}
+#' \item{v2}{a vertex}
+#' \item{w}{the weight of the edge linking v1 and v2}}
+#'    
+#' @source {Created in-house to serve as an example}  
+#' @examples
+#' data(wiki_graph)
+"wiki_graph"
